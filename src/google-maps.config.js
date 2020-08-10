df9d158e37a383ba07bc8ea260c318992d26e535
Andrew Roberts <andrew.jacob.roberts@gmail.com>
2020-08-10 15:05:51 -0400

    Update connection

--- src/google-maps.config.js
+++ src/google-maps.config.js
@@ -1,5 +1,4 @@
 const key = {};
-//const accessToken = "AIzaSyAMSRhlLzpVozeKgIxQmH12gVNOTSuKMNk"; // faa-scds-feeds-gui key, locked down to gh-pages URL
-const accessToken = "AIzaSyCy9siY2M8b2X_psZPrNtzV27i2Ka5u45c"; // ajr-maps-javascript, open
+const accessToken = "AIzaSyAMSRhlLzpVozeKgIxQmH12gVNOTSuKMNk"; // faa-scds-feeds-gui key, locked down to gh-pages URL
 
 export const googleMapsConfig = { accessToken, key };
