76dbfddf9b495d7d4be371e4b3742cb35906f98c
Andrew Roberts <andrew.jacob.roberts@gmail.com>
2020-09-28 12:53:44 -0400

    update fdps-feed

--- src/feed-fdps.js
+++ src/feed-fdps.js
@@ -22,35 +22,21 @@ function createTopicArray(topic) {
   return topic.split("/");
 }
 
-export function getRotatedIconUrl(fdpsPositionTick) {
-  const heading = radiansToDegrees(
-    Math.atan(Number(fdpsPositionTick.trackVelocityY) / Number(fdpsPositionTick.trackVelocityX))
-  );
-  let iconAngle = "0";
-  if (heading > 22.5 && heading < 67.5) {
-    iconAngle = "45";
+export function getRotationAngle(heading) {
+  let angle = 0;
+  try {
+    if (heading == 0) {
+      angle = 0;
+    } else {
+      angle = Math.round((Math.round(parseFloat(heading) / 10) / 10) * 100);
+      if (angle > 360) {
+        angle = 0;
+      }
+    }
+  } catch (e) {
+    angle = 0;
+    console.debug("Exception: " + e);
   }
-  if (heading > 67.5 && heading < 112.5) {
-    iconAngle = "90";
-  }
-  if (heading > 112.5 && heading < 157.5) {
-    iconAngle = "135";
-  }
-  if (heading > 157.5 && heading < 202.5) {
-    iconAngle = "180";
-  }
-  if (heading > 202.5 && heading < 247.5) {
-    iconAngle = "225";
-  }
-  if (heading > 247.5 && heading < 292.5) {
-    iconAngle = "270";
-  }
-  if (heading > 292.5 && heading < 337.5) {
-    iconAngle = "315";
-  }
-  return `images/plane-sprite/plane-${iconAngle}.png`;
-}
-
-function radiansToDegrees(radians) {
-  return radians * (180 / Math.PI);
+  console.debug("Angle: " + angle);
+  return angle;
 }
